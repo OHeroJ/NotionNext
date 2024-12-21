0526f3166f718ff39c508eb8ab3298eed4397d49
laijihua <1164258202@qq.com>
2024-12-21 22:23:16 +0800

    从数据库中获取

--- blog.config.js
+++ blog.config.js
@@ -477,11 +477,12 @@ const BLOG = {
   ADSENSE_GOOGLE_SLOT_IN_ARTICLE:
     process.env.NEXT_PUBLIC_ADSENSE_GOOGLE_SLOT_IN_ARTICLE || '3417694235', // Google AdScene>广告>按单元广告>新建文章内嵌广告 粘贴html代码中的data-ad-slot值
   ADSENSE_GOOGLE_SLOT_FLOW:
-    process.env.NEXT_PUBLIC_ADSENSE_GOOGLE_SLOT_FLOW || '7315606639', // Google AdScene>广告>按单元广告>新建信息流广告
+    process.env.NEXT_PUBLIC_ADSENSE_GOOGLE_SLOT_FLOW, // || '7315606639', // Google AdScene>广告>按单元广告>新建信息流广告
   ADSENSE_GOOGLE_SLOT_NATIVE:
     process.env.NEXT_PUBLIC_ADSENSE_GOOGLE_SLOT_NATIVE || '8137733710', // Google AdScene>广告>按单元广告>新建原生广告
   ADSENSE_GOOGLE_SLOT_AUTO:
     process.env.NEXT_PUBLIC_ADSENSE_GOOGLE_SLOT_AUTO, //|| '2943964249', Google AdScene>广告>按单元广告>新建展示广告 （自动广告）
+
 
   // 万维广告
   AD_WWADS_ID: process.env.NEXT_PUBLIC_WWAD_ID || null, // https://wwads.cn/ 创建您的万维广告单元ID
